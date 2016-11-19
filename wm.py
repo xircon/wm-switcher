5c7efc8fed2e826f4352478328d42587e9bb766d
xircon <xirconuk@gmail.com>
2016-11-19 12:48:49 +0000

    Update wm.py

--- wm.py
+++ wm.py
@@ -4,6 +4,7 @@ from PyQt4 import QtGui, QtCore
 
 import os
 import time
+import os.path
 
 
 class Window(QtGui.QWidget):
@@ -15,62 +16,70 @@ class Window(QtGui.QWidget):
         myFont = QtGui.QFont()
         myFont.setBold(True)
 
-        self.lbl1 = QtGui.QLabel('Metacity Window Manager', self)
-        self.lbl1.setFont(myFont)
-        self.lbl1.setAlignment(QtCore.Qt.AlignHCenter)
-        layout.addWidget(self.lbl1)
+        fname="/usr/bin/metacity"
+        if os.path.isfile(fname):
+            self.lbl1 = QtGui.QLabel('Metacity Window Manager', self)
+            self.lbl1.setFont(myFont)
+            self.lbl1.setAlignment(QtCore.Qt.AlignHCenter)
+            layout.addWidget(self.lbl1)
 
-        self.button1 = QtGui.QPushButton('Metacity - no compositor', self)
-        self.button1.clicked.connect(self.handleButton1)
-        layout.addWidget(self.button1)
+            self.button1 = QtGui.QPushButton('Metacity - no compositor', self)
+            self.button1.clicked.connect(self.handleButton1)
+            layout.addWidget(self.button1)
 
-        self.button2 = QtGui.QPushButton('Metacity + native compositing', self)
-        self.button2.clicked.connect(self.handleButton2)
-        layout.addWidget(self.button2)
+            self.button2 = QtGui.QPushButton('Metacity + native compositing', self)
+            self.button2.clicked.connect(self.handleButton2)
+            layout.addWidget(self.button2)
 
-        self.button3 = QtGui.QPushButton('Metacity + Compton', self)
-        self.button3.clicked.connect(self.handleButton3)
-        layout.addWidget(self.button3)
+            self.button3 = QtGui.QPushButton('Metacity + Compton', self)
+            self.button3.clicked.connect(self.handleButton3)
+            layout.addWidget(self.button3)
 
-        self.lbl2 = QtGui.QLabel('\nXFCE4 Window Manager', self)
-        self.lbl2.setFont(myFont)
-        self.lbl2.setAlignment(QtCore.Qt.AlignHCenter)
-        layout.addWidget(self.lbl2)
+        fname="/usr/bin/xfwm4"
+        if os.path.isfile(fname):
+            self.lbl2 = QtGui.QLabel('\nXFCE4 Window Manager', self)
+            self.lbl2.setFont(myFont)
+            self.lbl2.setAlignment(QtCore.Qt.AlignHCenter)
+            layout.addWidget(self.lbl2)
 
-        self.button4 = QtGui.QPushButton('xfwm4 - no compositor', self)
-        self.button4.clicked.connect(self.handleButton4)
-        layout.addWidget(self.button4)
+            self.button4 = QtGui.QPushButton('xfwm4 - no compositor', self)
+            self.button4.clicked.connect(self.handleButton4)
+            layout.addWidget(self.button4)
 
-        self.button5 = QtGui.QPushButton('xfwm4 + native compositing', self)
-        self.button5.clicked.connect(self.handleButton5)
-        layout.addWidget(self.button5)
+            self.button5 = QtGui.QPushButton('xfwm4 + native compositing', self)
+            self.button5.clicked.connect(self.handleButton5)
+            layout.addWidget(self.button5)
 
-        self.button6 = QtGui.QPushButton('xfwm4 + Compton', self)
-        self.button6.clicked.connect(self.handleButton6)
-        layout.addWidget(self.button6)
+            self.button6 = QtGui.QPushButton('xfwm4 + Compton', self)
+            self.button6.clicked.connect(self.handleButton6)
+            layout.addWidget(self.button6)
 
-        self.lbl3 = QtGui.QLabel('\nCompiz compositing WM', self)
-        self.lbl3.setFont(myFont)
-        self.lbl3.setAlignment(QtCore.Qt.AlignHCenter)
-        layout.addWidget(self.lbl3)
+        fname="/usr/bin/compiz"
+        if os.path.isfile(fname):
+            self.lbl3 = QtGui.QLabel('\nCompiz compositing WM', self)
+            self.lbl3.setFont(myFont)
+            self.lbl3.setAlignment(QtCore.Qt.AlignHCenter)
+            layout.addWidget(self.lbl3)
 
-        self.button7 = QtGui.QPushButton('compiz', self)
-        self.button7.clicked.connect(self.handleButton7)
-        layout.addWidget(self.button7)
+            self.button7 = QtGui.QPushButton('compiz', self)
+            self.button7.clicked.connect(self.handleButton7)
+            layout.addWidget(self.button7)
 
-        self.lbl4 = QtGui.QLabel('\nOpenbox Window Manager', self)
-        self.lbl4.setFont(myFont)
-        self.lbl4.setAlignment(QtCore.Qt.AlignHCenter)
-        layout.addWidget(self.lbl4)
+        fname="/usr/bin/openbox"
+        if os.path.isfile(fname):
+            self.lbl4 = QtGui.QLabel('\nOpenbox Window Manager', self)
+            self.lbl4.setFont(myFont)
+            self.lbl4.setAlignment(QtCore.Qt.AlignHCenter)
+            layout.addWidget(self.lbl4)
 
-        self.button8 = QtGui.QPushButton('openbox', self)
-        self.button8.clicked.connect(self.handleButton8)
-        layout.addWidget(self.button8)
+            self.button8 = QtGui.QPushButton('openbox', self)
+            self.button8.clicked.connect(self.handleButton8)
+            layout.addWidget(self.button8)
 
-        self.button9 = QtGui.QPushButton('openbox + Compton', self)
-        self.button9.clicked.connect(self.handleButton9)
-        #self.button9.setStyleSheet("background-color: green")
-        layout.addWidget(self.button9)
+            self.button9 = QtGui.QPushButton('openbox + Compton', self)
+            self.button9.clicked.connect(self.handleButton9)
+            #self.button9.setStyleSheet("background-color: green")
+            layout.addWidget(self.button9)
 
     def handleButton1(self):
         #Metacity no compositor
